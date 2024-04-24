30c42437a241cb87deada822ef22eae172dd417b
Vitalii Yefimchuk <yefimchukv@MacBook-Pro-Vitalii.local>
2024-04-24 13:40:57 +0200

    Edit

--- Formula/mergen.rb
+++ Formula/mergen.rb
@@ -2,7 +2,7 @@ class Mergen < Formula
   desc "Custom Mergen for CLI usage"
   homepage "https://github.com/Yefimchukv/mergen-cli"
   url "https://github.com/Yefimchukv/mergen-cli/archive/refs/tags/v1.0.0.zip"
-  sha256 "46487595b2eb006a027d04c5ee1c0c41ab0e0f7ea8feb12f51581a9eb38a610b"
+  sha256 "5493997bf6a38003a5573fec6b376bc2fe0292a451b14289c802507cbf009200"
   version "1.0.0"
 
   def install
