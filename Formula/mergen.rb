963345d8562c63b227251b666ea4b85748d0fbc6
Vitalii Yefimchuk <yefimchukv@MacBook-Pro-Vitalii.local>
2024-04-24 15:26:03 +0200

    Edit

--- Formula/mergen.rb
+++ Formula/mergen.rb
@@ -1,17 +1,11 @@
 class Mergen < Formula
   desc "Welcome to the Mergen CLI tool"
   homepage "https://github.com/Yefimchukv/mergen-cli"
-  url "https://github.com/Yefimchukv/mergen-cli/releases/download/v1.0.0/Mergen.pkg"
+  url "https://github.com/Yefimchukv/mergen-cli/releases/download/v1.0.0/mergen"
   sha256 "a4fea03b30f84c00a25c99d1a62d7aae0ecda23407d536dc693950ad0cde2799"
 
   def install
-    libexec.install "Mergen.pkg"
-  end
-
-  def caveats
-    <<~EOS
-      To finish the installation, run the following command with administrative privileges:
-      sudo installer -pkg #{libexec}/Mergen.pkg -target /
-    EOS
+    bin.install "mergen"
+    system "chmod", "+x", "#{bin}/mergen"
   end
 end
