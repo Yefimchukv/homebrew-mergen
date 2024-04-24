1f03f18b036f44bbb6bae988a949206c34d3238a
Vitalii Yefimchuk <yefimchukv@MacBook-Pro-Vitalii.local>
2024-04-24 14:59:24 +0200

    Edit

--- Formula/mergen.rb
+++ Formula/mergen.rb
@@ -1,5 +1,5 @@
 class Mergen < Formula
-  desc "A Mergen CLI tool"
+  desc "Welcome to the Mergen CLI tool"
   homepage "https://github.com/Yefimchukv/mergen-cli"
   url "https://github.com/Yefimchukv/mergen-cli/releases/download/v1.0.0/Mergen.pkg"
   sha256 "a4fea03b30f84c00a25c99d1a62d7aae0ecda23407d536dc693950ad0cde2799"
