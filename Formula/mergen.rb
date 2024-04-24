476926cf1046fb6b6362c2c05b3405a2bfe5aaad
Vitalii Yefimchuk <yefimchukv@MacBook-Pro-Vitalii.local>
2024-04-24 16:27:32 +0200

    Edit

--- Formula/mergen.rb
+++ Formula/mergen.rb
@@ -1,7 +1,7 @@
 class Mergen < Formula
   desc "Welcome to the Mergen CLI tool"
   homepage "https://github.com/Yefimchukv/mergen-cli"
-  url "https://github.com/Yefimchukv/mergen-cli/releases/download/v1.0.0/Mergen-signed"
+  url "https://github.com/Yefimchukv/mergen-cli/releases/download/1.0.0/Mergen-signed"
   sha256 "3c1d027cfa4feb090ddebddaf611cb8602bdce044fef30fe1e083aba2b80132c"
 
   def install
