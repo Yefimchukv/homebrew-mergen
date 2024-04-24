128e628756807fd2c6213f092763af68076d5ff6
Vitalii Yefimchuk <yefimchukv@MacBook-Pro-Vitalii.local>
2024-04-24 15:21:48 +0200

    Edit

--- Formula/mergen.rb
+++ Formula/mergen.rb
@@ -5,6 +5,13 @@ class Mergen < Formula
   sha256 "a4fea03b30f84c00a25c99d1a62d7aae0ecda23407d536dc693950ad0cde2799"
 
   def install
-      system "installer", "-pkg", "Mergen.pkg", "-target", "/"
+    libexec.install "Mergen.pkg"
+  end
+
+  def caveats
+    <<~EOS
+      To finish the installation, run the following command with administrative privileges:
+      sudo installer -pkg #{libexec}/Mergen.pkg -target /
+    EOS
   end
 end
