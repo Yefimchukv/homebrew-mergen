1c806f4e58bfb9535e1a6ab8d68f20dfb6fdeb76
Vitalii Yefimchuk <yefimchukv@MacBook-Pro-Vitalii.local>
2024-04-24 13:44:48 +0200

    Edit

--- Formula/mergen.rb
+++ Formula/mergen.rb
@@ -6,7 +6,7 @@ class Mergen < Formula
   version "1.0.0"
 
   def install
-    system "unzip", "mergen-cli-1.0.0.zip"
+    system "unzip", "10c46a0065460dcf7b77af057471eceb8f9686f726156191be70d78fe650985b--mergen-cli-1.0.0.zip"
     bin.install "mergen"
   end
 end
