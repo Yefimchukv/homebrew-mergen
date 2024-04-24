b13b9efe49344132641a8fbfd160d729b67e44ff
Vitalii Yefimchuk <yefimchukv@MacBook-Pro-Vitalii.local>
2024-04-24 13:02:32 +0200

    Updated Mergen formula

--- Formula/mergen.rb
+++ Formula/mergen.rb
@@ -3,6 +3,7 @@ class Mergen < Formula
   homepage "https://github.com/Yefimchukv/mergen-cli"
   url "https://github.com/Yefimchukv/mergen-cli/blob/main/Mergen-signed.pkg"
   sha256 "a4fea03b30f84c00a25c99d1a62d7aae0ecda23407d536dc693950ad0cde2799"
+  version "1.0.0"
 
   def install
     system "installer", "-pkg", "mergen.pkg", "-target", "/"
