55107d13d466d07a4464090dfa8bd5862a486474
Vitalii Yefimchuk <yefimchukv@MacBook-Pro-Vitalii.local>
2024-04-24 13:31:47 +0200

    Edit

--- Formula/mergen.rb
+++ Formula/mergen.rb
@@ -6,6 +6,6 @@ class Mergen < Formula
   version "1.0.0"
 
   def install
-    system "installer", "-pkg", "mergen.pkg", "-target", "/"
+    system "installer", "-pkg", "Mergen.pkg", "-target", "/"
   end
 end
